9ff8c37acddb281a38f51608553a20803a4cc472
Joao Victor de Souza Santos <joaovictor.souz@gmail.com>
2020-09-21 15:02:05 -0300

    security changes

--- src/main/java/br/com/brasilprev/api/config/AuthorizationServerConfig.java
+++ src/main/java/br/com/brasilprev/api/config/AuthorizationServerConfig.java
@@ -33,7 +33,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 				.secret("r$s&i0#")
 				.scopes("read", "write")
 				.authorizedGrantTypes("password", "refresh_token")
-				.accessTokenValiditySeconds(20)
+				.accessTokenValiditySeconds(60)
 				.refreshTokenValiditySeconds(3600 * 24);
 	}
 	
---
+++ src/main/java/br/com/brasilprev/api/config/filter/CorsFilter.java
@@ -0,0 +1,51 @@
+package br.com.brasilprev.api.config.filter;
+
+import br.com.brasilprev.api.property.ApplicationProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
+	
+	@Autowired
+	private ApplicationProperties applicationProperties;
+	
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse resp = (HttpServletResponse) response;
+		
+		//enviar sempre, em todas as requisicoes
+		resp.setHeader("Access-Control-Allow-Origin", applicationProperties.getOriginAllowed());
+		resp.setHeader("Access-Control-Allow-Credentials", "true");
+		
+		if("OPTIONS".equals(req.getMethod()) && applicationProperties.getOriginAllowed().equals(req.getHeader("Origin"))) {
+			resp.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, OPTIONS");
+			resp.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept, HEADER1");
+			resp.setHeader("Access-Control-Max-Age", "3600");
+			resp.setStatus(HttpServletResponse.SC_OK);
+		} else {
+			chain.doFilter(req, resp);
+		}
+	}
+	
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		
+	}	
+
+	@Override
+	public void destroy() {
+		
+	}
+
+}
